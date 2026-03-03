a2f0a8a15dbb204daf0e008bf78b5cd1c48e6475
fneel <70952301+fneel@users.noreply.github.com>
2026-03-03 15:34:46 +0100

    Home - header layout with lipsum images #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,44 @@
-export default function Home() {}
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex max-w-7xl gap-10 justify-center mx-auto">
+      <section className="flex basis-4/10 flex-col p-6 ">
+        <div>
+          <h1 className="text-8xl">
+            <span className="text-xl">small heading goes here</span>
+            <br></br> & LARGE HEADING
+          </h1>
+        </div>
+
+        <div className="flex justify-between mb-0 mt-auto">
+          <Link href={"/"}>
+            <Image
+              alt=""
+              src={"https://picsum.photos/300/300"}
+              width={200}
+              height={200}
+            ></Image>
+          </Link>
+          <Link href={"/"}>
+            <Image
+              alt=""
+              src={"https://picsum.photos/300/300"}
+              width={200}
+              height={200}
+            ></Image>
+          </Link>
+        </div>
+      </section>
+
+      <Image
+        className="basis-6/10 p-6"
+        alt=""
+        src={"https://picsum.photos/300/250"}
+        width={300}
+        height={250}
+      ></Image>
+    </div>
+  );
+}
