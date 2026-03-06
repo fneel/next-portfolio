be8fd3589e31c9a5fe5a400ff972f1e83985bcbf
fneel <70952301+fneel@users.noreply.github.com>
2026-03-06 10:39:05 +0100

    fixed type error in Button

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
+import { ReactNode } from "react";
+
+type ButtonVariant = "primary" | "secondary" | "tertiary";
+
+interface ButtonProps {
+  href?: string;
+  children?: ReactNode;
+  variant?: ButtonVariant;
+  className?: string;
+}
 
 export default function Button({
   href = "",
-  children = "",
+  children,
   variant = "primary",
   className = "",
   ...props
-}) {
+}: ButtonProps) {
   const baseStyles =
-    "inline-flex items-center justify-center px-8 py-2 rounded-3xl font-normal  transition-all duration-200 w-fit tracking-wide hover:shadow-[0_0_35px_8px_rgba(210,_255,_31,_1)]";
+    "inline-flex items-center justify-center px-8 py-2 rounded-3xl font-normal transition-all duration-200 w-fit tracking-wide hover:shadow-[0_0_35px_8px_rgba(210,_255,_31,_1)]";
 
   const variants = {
-    primary: "bg-zinc-900 text-zinc-100 ",
+    primary: "bg-zinc-900 text-zinc-100",
     secondary: "bg-zinc-100 text-zinc-900 hover:bg-gray-600",
-    tertiary: "bg-zinc-100 text-zinc-900 shadow-md   hover:shadow-lg",
+    tertiary: "bg-zinc-100 text-zinc-900 shadow-md hover:shadow-lg",
   };
 
   return (
