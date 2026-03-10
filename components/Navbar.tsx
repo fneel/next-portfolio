4bea12e5d9f796bb5a0d90a3acb86872ea0b53a3
fneel <70952301+fneel@users.noreply.github.com>
2026-03-10 14:50:06 +0100

    fixing vercel complaints

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,14 +10,13 @@ export default function MainNav() {
     <nav className="sticky h-15 top-0 left-0 z-50 w-full flex justify-center shadow-sm bg-mist-100">
       <div className="flex container items-center my-2 justify-between">
         <Link href="/" className="bg-[#D2FF1F] rounded-full">
-          {" "}
           <Image
             className="h-12 w-auto"
             alt=""
             height={50}
             width={50}
             src="/el-in.png"
-          ></Image>
+          />
         </Link>
         <ul className="flex min-w-1/3 justify-between [font-variant:small-caps] ">
           <li>
