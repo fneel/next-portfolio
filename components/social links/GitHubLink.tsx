bfc89b821b77fc5c27312508faa94cb914965aba
fneel <70952301+fneel@users.noreply.github.com>
2026-03-11 12:59:40 +0100

    updated tag names from GTM

--- components/social links/GitHubLink.tsx
+++ components/social links/GitHubLink.tsx
@@ -11,7 +11,7 @@ const GitHubLink = () => {
     <a
       onClick={() =>
         sendGTMEvent({
-          event: "github_link",
+          event: "social_link",
           page_location: path,
         })
       }
